4d6bea14ccebb1ba2e02d40b2316443597612e23
FroshTheFrog <28665112+FroshTheFrog@users.noreply.github.com>
2022-04-25 00:32:16 -0400

    rollout now returns index

--- src/implementations/constants.rs
+++ src/implementations/constants.rs
@@ -6,4 +6,4 @@ pub const STATE_VALUE_MAX : i32 = 5000;
 pub const NODE_VALUE_MIN : i32 = -10;
 pub const NODE_VALUE_MAX : i32 = 10;
 
-pub const ROLLOUT_DEPTH : i32 = 3;
+pub const ROLLOUT_DEPTH : i32 = 2;
--- src/implementations/rollout_strategy.rs
+++ src/implementations/rollout_strategy.rs
@@ -7,15 +7,29 @@ pub fn rollout_strategy(
     state: StateArray,
     mutations: &Vec<Box<Mutation<StateArray>>>,
     tree : &Box<dyn EvaluationTree<StateArray>>
-    ) -> i32 {
+    ) -> usize {
 
     // Keep track of where we currently care so that it does not get stuck in local minima.
     let start_value = tree.evaluate(state);
 
-    lets_rollout(state, mutations, tree, ROLLOUT_DEPTH, start_value)
+    let mut best_value = i32::min_value();
+    let mut best_index = 0;
+
+    for index in 0..mutations.len() {
+        let new_state = mutations[index](state);
+
+        let state_value = get_rollout_Value(new_state, mutations, tree, ROLLOUT_DEPTH, start_value);
+
+        if state_value > best_value {
+            best_value = state_value;
+            best_index = index;
+        }
+    }
+
+    best_index
 }
 
-fn lets_rollout(
+fn get_rollout_Value(
     state: StateArray,
     mutations: &Vec<Box<Mutation<StateArray>>>,
     tree : &Box<dyn EvaluationTree<StateArray>>,
@@ -32,15 +46,15 @@ fn lets_rollout(
     for mutation in mutations {
         let new_state = mutation(state);
 
-        let down_the_tree = lets_rollout(new_state, mutations, tree, depth - 1, start_value);
-
-        best_value = cmp::max(best_value, down_the_tree);
-
         let current_state_value = tree.evaluate(new_state);
 
         if current_state_value > best_value && current_state_value != start_value {
             best_value = current_state_value;
         }
+
+        let down_the_tree_value = get_rollout_Value(new_state, mutations, tree, depth - 1, start_value);
+
+        best_value = cmp::max(best_value, down_the_tree_value);
     }
 
     best_value
