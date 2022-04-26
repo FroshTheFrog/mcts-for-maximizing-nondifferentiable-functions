e15ad9f1836a3300fac63338c155364698a50556
FroshTheFrog <28665112+FroshTheFrog@users.noreply.github.com>
2022-04-26 12:42:41 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,6 @@ mod tree_search;
 
 use crate::{types::State, implementations::{state_array, evaluations_tree, rollout_strategy::rollout_strategy}, tree_search::search};
 
-/*
-    stat_state : T,
-    rollout : RollOut<T>,
-    tree : &Box<dyn EvaluationTree<T>>,
-    loops : u32,
-    rollout_epsilon : f64,
-    uct_exploration : f64,
-*/
 
 fn main() {
     let start_state = state_array::StateArray::random_state();
@@ -20,5 +12,5 @@ fn main() {
 
     let searched_state = search(start_state, rollout_strategy, &tree, 500, 0.3, 2.0);
 
-    println!("{}", tree.evaluate(start_state));
+    println!("{}", tree.evaluate(searched_state));
 }
